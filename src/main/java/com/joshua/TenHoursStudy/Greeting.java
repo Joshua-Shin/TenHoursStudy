2163478d7fe4bcc716279328a67cbd20e48573b9
sjh910805@gmail.com <sjh910805@gmail.com>
2023-02-03 21:41:09 +0900

    feat: restapi 간단히 구현

---
+++ src/main/java/com/joshua/TenHoursStudy/Greeting.java
@@ -0,0 +1,11 @@
+package com.joshua.TenHoursStudy;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class Greeting {
+    private final long id;
+    private final String content;
+}
